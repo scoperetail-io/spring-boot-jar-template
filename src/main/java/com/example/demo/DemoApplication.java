d8a0f668f7211ee0c00eb3bf557a3fbd8f4a05e7
scoperetail2 <felix.diaz@scoperetail.com>
2021-06-17 20:47:18 -0500

    SCOOP-14562 first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+/*-
+ * *****
+ * demo
+ * -----
+ * Copyright (C) 2018 - 2021 Scope Retail Systems Inc.
+ * -----
+ * This software is owned exclusively by Scope Retail Systems Inc.
+ * As such, this software may not be copied, modified, or
+ * distributed without express permission from Scope Retail Systems Inc.
+ * =====
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+/*-
+ * *****
+ * demo
+ * -----
+ * Copyright (C) 2018 - 2021 Scope Retail Systems Inc.
+ * -----
+ * This software is owned exclusively by Scope Retail Systems Inc.
+ * As such, this software may not be copied, modified, or
+ * distributed without express permission from Scope Retail Systems Inc.
+ * =====
+ */
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
